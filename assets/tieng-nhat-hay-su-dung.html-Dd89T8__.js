c1b5181ff02fda7ee2cb433cd43a47b60e4b12c4
linhtd87 <106981400+linhtd87@users.noreply.github.com>
2024-08-14 15:43:46 +0000

    Deploying to gh-pages from @ linhtd87/linhtd87.github.io@3740b9b5ac16b64d0805014bd43063dea0231501 🚀

---
+++ assets/tieng-nhat-hay-su-dung.html-Dd89T8__.js
@@ -0,0 +1,22 @@
+import{_ as t}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as e,o as n,d as a}from"./app-BxSKvB-8.js";const r={},i=a(`<h2 id="cac-tu-thuong-gap" tabindex="-1"><a class="header-anchor" href="#cac-tu-thuong-gap"><span>Các từ thường gặp</span></a></h2><pre><code>có thể あり 
+giải trình 説明
+không なし 
+tiêu đề タイトル
+trống 空白 
+</code></pre><h2 id="mau" tabindex="-1"><a class="header-anchor" href="#mau"><span>Màu</span></a></h2><pre><code>đỏ 赤い akai 
+trắng 白 shiro 
+xám 灰色 haiiro 
+</code></pre><h2 id="ki-tu-nhat" tabindex="-1"><a class="header-anchor" href="#ki-tu-nhat"><span>Kí tự nhật</span></a></h2><pre><code>-
+○
+×
+■
+●
+・
+※
+－
+</code></pre><h2 id="so-nhat" tabindex="-1"><a class="header-anchor" href="#so-nhat"><span>Số nhật</span></a></h2><pre><code>⓪ ① ② ③ ④ ⑤ ⑥ ⑦ ⑧ ⑨ 
+⑩ ⑪ ⑫ ⑬ ⑭ ⑮ ⑯ ⑰ ⑱ ⑲ 
+⑳ ㉑ ㉒ ㉓ ㉔ ㉕ ㉖ ㉗ ㉘ ㉙ 
+㉚ ㉛ ㉜ ㉝ ㉞ ㉟ ㊱ ㊲ ㊳ ㊴ 
+㊵ ㊶ ㊷ ㊸ ㊹ ㊺ ㊻ ㊼ ㊽ ㊾ ㊿
+</code></pre>`,8),o=[i];function h(c,p){return n(),e("div",null,o)}const d=t(r,[["render",h],["__file","tieng-nhat-hay-su-dung.html.vue"]]),l=JSON.parse('{"path":"/pages/Nh%E1%BA%ADt%20b%E1%BA%A3n/tieng-nhat-hay-su-dung.html","title":"Tiếng nhật hay sử dụng (JA ja jp)","lang":"vi-VN","frontmatter":{"title":"Tiếng nhật hay sử dụng (JA ja jp)","cover":"/assets/images/cover-japan.jpg","icon":"file","order":1,"author":"Mr.Linh","date":"2020-01-01T00:00:00.000Z","category":["Tra cứu"],"tag":["Tra cứu Tiếng nhật","Ja","ja","Jp","jp"],"sticky":false,"star":true,"footer":"Footer content for test","copyright":"No Copyright","description":"Các từ thường gặp Màu Kí tự nhật Số nhật","head":[["meta",{"property":"og:url","content":"https://linhtd.dev/pages/Nh%E1%BA%ADt%20b%E1%BA%A3n/tieng-nhat-hay-su-dung.html"}],["meta",{"property":"og:site_name","content":"linhtd.dev"}],["meta",{"property":"og:title","content":"Tiếng nhật hay sử dụng (JA ja jp)"}],["meta",{"property":"og:description","content":"Các từ thường gặp Màu Kí tự nhật Số nhật"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:image","content":"https://linhtd.dev/assets/images/cover-japan.jpg"}],["meta",{"property":"og:locale","content":"vi-VN"}],["meta",{"property":"og:updated_time","content":"2023-11-06T03:35:36.000Z"}],["meta",{"name":"twitter:card","content":"summary_large_image"}],["meta",{"name":"twitter:image:src","content":"https://linhtd.dev/assets/images/cover-japan.jpg"}],["meta",{"name":"twitter:image:alt","content":"Tiếng nhật hay sử dụng (JA ja jp)"}],["meta",{"property":"article:author","content":"Mr.Linh"}],["meta",{"property":"article:tag","content":"Tra cứu Tiếng nhật"}],["meta",{"property":"article:tag","content":"Ja"}],["meta",{"property":"article:tag","content":"ja"}],["meta",{"property":"article:tag","content":"Jp"}],["meta",{"property":"article:tag","content":"jp"}],["meta",{"property":"article:published_time","content":"2020-01-01T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2023-11-06T03:35:36.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"Tiếng nhật hay sử dụng (JA ja jp)\\",\\"image\\":[\\"https://linhtd.dev/assets/images/cover-japan.jpg\\"],\\"datePublished\\":\\"2020-01-01T00:00:00.000Z\\",\\"dateModified\\":\\"2023-11-06T03:35:36.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Mr.Linh\\"}]}"]]},"headers":[{"level":2,"title":"Các từ thường gặp","slug":"cac-tu-thuong-gap","link":"#cac-tu-thuong-gap","children":[]},{"level":2,"title":"Màu","slug":"mau","link":"#mau","children":[]},{"level":2,"title":"Kí tự nhật","slug":"ki-tu-nhat","link":"#ki-tu-nhat","children":[]},{"level":2,"title":"Số nhật","slug":"so-nhat","link":"#so-nhat","children":[]}],"git":{"createdTime":1699241736000,"updatedTime":1699241736000,"contributors":[{"name":"Trinh Duy Linh","email":"linh.td@trans-tech.vn","commits":1}]},"readingTime":{"minutes":0.52,"words":157},"filePathRelative":"pages/Nhật bản/tieng-nhat-hay-su-dung.md","localizedDate":"1 tháng 1 năm 2020","excerpt":"<h2>Các từ thường gặp</h2>\\n<pre><code>có thể あり \\ngiải trình 説明\\nkhông なし \\ntiêu đề タイトル\\ntrống 空白 \\n</code></pre>\\n<h2>Màu</h2>\\n<pre><code>đỏ 赤い akai \\ntrắng 白 shiro \\nxám 灰色 haiiro \\n</code></pre>\\n<h2>Kí tự nhật</h2>\\n<pre><code>-\\n○\\n×\\n■\\n●\\n・\\n※\\n－\\n</code></pre>\\n<h2>Số nhật</h2>\\n<pre><code>⓪ ① ② ③ ④ ⑤ ⑥ ⑦ ⑧ ⑨ \\n⑩ ⑪ ⑫ ⑬ ⑭ ⑮ ⑯ ⑰ ⑱ ⑲ \\n⑳ ㉑ ㉒ ㉓ ㉔ ㉕ ㉖ ㉗ ㉘ ㉙ \\n㉚ ㉛ ㉜ ㉝ ㉞ ㉟ ㊱ ㊲ ㊳ ㊴ \\n㊵ ㊶ ㊷ ㊸ ㊹ ㊺ ㊻ ㊼ ㊽ ㊾ ㊿\\n</code></pre>","autoDesc":true}');export{d as comp,l as data};
